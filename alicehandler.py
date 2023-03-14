fbc6621608c1448c8b85ea41114aa238a4f8c1e5
mikolainer <mikolainer@mail.ru>
2023-03-14 20:24:17 +0300

    mini refactoring

--- alicehandler.py
+++ alicehandler.py
@@ -19,34 +19,26 @@ Meet = Meeting()
 
 Datarequest = datarequest()
 
-
-
-def handler(event,context):
-
-    
-
-    """
-    Entry-point for Serverless Function.
-    :param event: request payload.
-    :param context: information about current execution context.
-    :return: response to be serialized as JSON.
-    """
-  
-    meetlist = Meet.responseToMeetState(event['session']['new'])
-
-    if meetlist != None:
-        Datarequest.isShtut = "false"
-        return {
+def make_response(text, end = False):
+    response_template = {
             'version': event['version'],
             'session': event['session'],
             #"session_state": session_state,
             'response': {
-                # Respond with the original request or welcome the user if this is the beginning of the dialog and the request has not yet been made.
-                'text': meetlist[0],
-                # Don't finish the session after this response.
-                'end_session': meetlist[1]
+                'text': text,
+                'end_session': end
             }
         }
+
+# start point
+def handler(event,context):
+    meetlist = Meet.responseToMeetState(event['session']['new'])
+
+    
+
+    if meetlist != None:
+        Datarequest.isShtut = "false"
+        return make_response(meetlist[0], meetlist[1])
 
     if 'request' in event and \
             'original_utterance' in event['request'] \
@@ -60,30 +52,26 @@ def handler(event,context):
       
 
         
-        #bot.send_message(-1001609876238 , f"тест говна",message_thread_id = 453)
+
         #первое это номер чата
-        flag = "not none"
+        screen = "screen" in event["meta"]["interfaces"]
         #print(type(event["meta"]["interfaces"]["screen"]))
-        if event["meta"]["interfaces"]["screen"] == {}:
-            flag = "none"
-        msg = "session id: " + event["session"]["session_id"] + "\n\n" + "user_id: " + event["session"]["user"]["user_id"] + "\n\n" + "screen: " + flag + "\n\n" + "request: " + str(event['request']['command']) + "\n\n" + "response: "+ list_arg[0]
+        #if event["meta"]["interfaces"]["screen"] == {}:
+        #    flag = "none"
+        msg =   "session id: " + event["session"]["session_id"] + "\n\n"
+        msg +=  "user_id: " + event["session"]["user"]["user_id"] + "\n\n"
+        msg += "screen: " + str(screen) + "\n\n"
+        msg += "request: " + str(event['request']['command']) + "\n\n"
+        msg += "response: "+ list_arg[0]
+        if (len(list_arg) > 2)
+            msg += "debug: "+ list_arg[2]
         bot.send_message(-1001609876238 , msg ,message_thread_id = 453)
 
 
     #else:
         #session_state['state'] = 1
     
-        return {
-            'version': event['version'],
-            'session': event['session'],
-            #"session_state": session_state,
-            'response': {
-                # Respond with the original request or welcome the user if this is the beginning of the dialog and the request has not yet been made.
-                'text': list_arg[0],
-                # Don't finish the session after this response.
-                'end_session': list_arg[1]
-            }
-        }
+        return make_response(list_arg[0],list_arg[1])
     
 
 context = None
