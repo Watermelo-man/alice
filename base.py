6249482b6e9cc06ffa3f717c2b70c8fe44d79a6f
Watermelo-man <watermelo.man@yandex.ru>
2023-03-16 23:45:07 +0300

    add timeout

--- base.py
+++ base.py
@@ -5,14 +5,15 @@ class Base():
     
     def __init__(self) -> None:
             pass
-    def connect(self,base_source = "alisa_gamerules"):
+    def connect(self,base_source = "alisa_gamerules",timeout_read = 1):
             try:
                 conn = pymysql.connect(
                     user = "user",
                     password="uJ9_ZIOavQ",
                     host="185.171.192.30",
                     port=3306,
-                    database=base_source
+                    database=base_source,
+                    read_timeout = timeout_read
                 )
                 self.base = conn
                 return 1 #"подключение успешно!"
